e12c409352051d1182240f385a8bcfce572357dd
Cher-iF <112823662+Cher-iF@users.noreply.github.com>
2022-12-28 03:12:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,10 @@ import os
 class DefaultConfig:
     """Configuration for the bot."""
 
-    PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "45c98dc5-6ad6-4693-a51f-89d28ffc032a")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "315164affd6148cb9530f673610b291e")
-    #LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "https://p10luisapps-authoring.cognitiveservices.azure.com/")
-    APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "f3a09437-aa25-4288-b17f-82f8318e4471"
-    )
+    PORT = 8000
+    APP_ID = "2feaa656-47a8-42c8-b2da-9d4fa467d2ba"
+    APP_PASSWORD = "dzh8Q~8~UUQlQ2ka5PuMmcq2SjchCG1Khc9v1bfo"
+    LUIS_APP_ID = "45c98dc5-6ad6-4693-a51f-89d28ffc032a"
+    LUIS_API_KEY = "315164affd6148cb9530f673610b291e"
+    LUIS_API_HOST_NAME = "https://p10luisapps-authoring.cognitiveservices.azure.com/"
+    APPINSIGHTS_INSTRUMENTATION_KEY = "f3a09437-aa25-4288-b17f-82f8318e4471"
